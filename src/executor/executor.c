90eb451b312f09ba5f561478a70fe986c19c16dd
gabref <gabrielfranzer@gmail.com>
2024-08-01 13:38:17 +0200

    fix: exit checks on arguments

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -209,16 +209,27 @@ void	builtin_exit(t_minishell *ms, char **args)
 	i = 0;
 	if (args && args[0] && args[1])
 	{
-		ft_putstr_fd("too many arguments\n", STDERR_FILENO);
+		ft_putstr_fd(" too many arguments\n", STDERR_FILENO);
+		ms->last_exit_status = 1;
 		return ;
 	}
 	if (args && args[0])
 	{
+		if (ft_strchr(args[0], '+'))
+		{
+			ms->exit_ms = 100;
+			return ;
+		}
+		if (ft_strchr(args[0], '-'))
+		{
+			ms->exit_ms = 156;
+			return ;
+		}
 		while (args[0][i])
 		{
 			if (!ft_isdigit(args[0][i]))
 			{
-				ft_putstr_fd("numeric arguements required\n", STDERR_FILENO);
+				ft_putstr_fd(" numeric argument required\n", STDERR_FILENO);
 				ms->exit_ms = 2;
 				return ;
 			}
