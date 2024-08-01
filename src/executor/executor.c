76d82ac5e7f43a66dfc8132c0de251dcca515692
gabref <gabrielfranzer@gmail.com>
2024-08-01 13:31:00 +0200

    fix: export missing checks

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -180,6 +180,12 @@ void	builtin_export(t_minishell *ms, char **args)
 	}
 	if (args && args[0] && args[1])
 		return ;
+	if (ft_strchr(args[0], '-') || !ft_isalpha(args[0][0]) || args[0][0] == '=')
+	{
+		ft_putstr_fd(" not a valid identifier\n", STDERR_FILENO);
+		ms->last_exit_status = 1;
+		return ;
+	}
 	if (ft_strchr(args[0], '='))
 	{
 		splitted = ft_split(args[0], '=');
